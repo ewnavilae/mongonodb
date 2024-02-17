37dc1048ac1bc100192684976517e8bfae8ee613
Miguel Brandão <ewnavilae@gmail.com>
2024-02-17 02:24:29 +0000

    Add more snake

--- db.py
+++ db.py
@@ -2,10 +2,11 @@ def d(n):
  for x in range(2,n):
   for y in range(2,x):
    if x%y==0:break
-  else:print(x)
+  else:print()
 def b(n):
  for x in range(2,n):
   for y in range(2,x):
    if x%y==0:break
-  else:print(x)
-p(int(input()))
+  else:print()
+d(int(4))
+b(int(2))
