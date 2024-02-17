3e23175f08f0d6cfbe72ba95f0bcf56f0eef8f8d
Miguel Brandão <ewnavilae@gmail.com>
2024-02-17 02:01:15 +0000

    Add snake db version

---
+++ db.py
@@ -0,0 +1,11 @@
+def d(n):
+ for x in range(2,n):
+  for y in range(2,x):
+   if x%y==0:break
+  else:print(x)
+def b(n):
+ for x in range(2,n):
+  for y in range(2,x):
+   if x%y==0:break
+  else:print(x)
+p(int(input()))
